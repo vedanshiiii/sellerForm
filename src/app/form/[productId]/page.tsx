1760dfb8a80f9408b0535455565d90809e87b686
aashiejain13 <aashiejain13@gmail.com>
2025-05-24 15:22:36 +0530

    morree

--- src/app/form/[productId]/page.tsx
+++ src/app/form/[productId]/page.tsx
@@ -3,7 +3,7 @@
 import { Toaster } from 'react-hot-toast';
 import SellerForm from '@/components/SellerForm';
 
-export default function Page({ params }: { params: { productId: string } }) {
+export default async function Page({ params }: { params: { productId: string } }) {
   return (
     <main className="min-h-screen bg-gray-50">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
